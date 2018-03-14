3c66aaeadb95cadefc628c302d97ce6991ae8945
bastinc <christophe.bastin@umons.ac.be>
2018-03-14 15:24:48 +0100

    add rsync script

---
+++ pidas/rsync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+USERNAME=''
+SERVER_NAME=''
+SSH_PORT=
+LOCAL_FILES='/home/pi/pidas/pidas/data/data_log.*'
+REMOTE_FOLDER='/home/memoris/raw_data'
+LOG_FILE_PATH='/home/pi/rsync_log'
+
+
+/usr/bin/rsync -avz -h -e  "ssh -p $SSH_PORT" $LOCAL_FILES $USERNAME@$SERVER_NAME:$REMOTE_FOLDER >> $LOG_FILE_PATH 2>&1
+
