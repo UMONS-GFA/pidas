ea133e80d65d98cfda6e2cda1464689d1052fd1f
bastinc <christophe.bastin@umons.ac.be>
2018-06-20 11:19:24 +0200

    sync log files

--- pidas/rsync_example.sh
+++ pidas/rsync_example.sh
@@ -4,9 +4,13 @@ USERNAME=''
 SERVER_NAME=''
 SSH_PORT=
 LOCAL_FILES='/home/pi/pidas/pidas/data/data_log.*'
+LOCAL_LOG_FILES='/home/pi/pidas/pidas/logs/raspardas_log.*'
 REMOTE_FOLDER='/home/memoris/raw_data'
+REMOTE_LOG_FOLDER='/home/memoris/logs/memoris1'
 LOG_FILE_PATH='/home/pi/pidas/pidas/rsync_log'
 
-
+# sync data files
 /usr/bin/rsync --remove-source-files -avz -h -e  "ssh -p $SSH_PORT" $LOCAL_FILES $USERNAME@$SERVER_NAME:$REMOTE_FOLDER >> $LOG_FILE_PATH 2>&1
+#sync log files
+/usr/bin/rsync -avz -h -e  "ssh -p $SSH_PORT" $LOCAL_LOG_FILES $USERNAME@$SERVER_NAME:$REMOTE_LOG_FOLDER >> $LOG_FILE_PATH 2>&1
 
